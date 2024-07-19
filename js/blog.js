8cc4b347d61ab4fd70570bb7cdb38baed8948641
brilianaringga <brilianaringga@gmail.com>
2024-07-19 14:31:10 +0700

    add blog posts

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,5 @@
+localStorage.setItem("pages", "1");
+
 const parsing_categories = (data) => {
   let element = document.getElementById("categories-container");
   let categories = "";
@@ -129,6 +131,82 @@ const get_tags = () => {
     });
 };
 
+// tags
+const generate_post_component = (data) => {
+  let result = "";
+
+  if (data.type == "ads") {
+    result = `
+            <div class="card mb-3 p-3 bg-warning-subtle">
+                <span class="fw-bold fs-5 fst-italic">
+                    ${data.text}
+                </span>
+                <span class="text-end">${data.author}</span>
+            </div>
+        `;
+  } else {
+    result = `
+            <div class="card mb-3 bg-warning-subtle">
+                <img
+                    src="${data.imageUrl}"
+                    class="card-img-top"
+                    alt="blog-photo"
+                />
+                <div class="card-body">
+                    <h5 class="card-title">
+                        ${data.title}
+                </span>
+                    </h5>
+                    <p class="card-text">
+                        ${data.text}
+                </span>
+                    </p>
+                    <div class="d-flex justify-content-between">
+                        <span
+                        ><i class="fa-solid fa-heart text-danger"></i>15</span
+                        >
+                        <span>by ${data.author} / ${data.comments} comments / ${data.date} </span>
+                    </div>
+                </div>
+            </div>
+        `;
+  }
+
+  return result;
+};
+
+const parsing_blogs = (data) => {
+  let element1 = document.getElementById("posts-container1");
+  let element2 = document.getElementById("posts-container2");
+  let col1 = "";
+  let col2 = "";
+
+  for (let index = 0; index < data.length; index++) {
+    if (index % 2 == 0) {
+      col1 += generate_post_component(data[index]);
+    } else {
+      col2 += generate_post_component(data[index]);
+    }
+  }
+
+  element1.innerHTML = col1;
+  element2.innerHTML = col2;
+};
+
+const get_blogs = () => {
+  fetch("data/posts.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      parsing_blogs(data.data);
+      data_local = data.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 get_categories();
 
 get_archieves();
@@ -136,3 +214,5 @@ get_archieves();
 get_popularpost();
 
 get_tags();
+
+get_blogs();
